d611d22bd7cf520165a698efdad1dc77a02e4585
micheal-donald <michaelmonyenye@gmail.com>
2023-02-27 17:44:32 +0300

    add git tag script

---
+++ build/git_update.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# This script is used to update the git repository
+
+VERSION=""
+
+# Get parameters
+while getopts v: opt; 
+do
+    case "${opt}" in
+        v)
+            VERSION=$OPTARG;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1;;
+    esac
+done
+
+#get highest version and addv0.01.0 if no version is given
+git fetch --prune --unshallow 2> /dev/null
+CURRENT_VERSION=$(git describe --tags --abbrev=0) 2> /dev/null
+
+if [ $CURRENT_VERSION == '' ]; 
+then
+    CURRENT_VERSION="v0.1.0"
+fi
+echo "Current version: $CURRENT_VERSION"
+
+#replace . with space and split into array
+CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
+
+# get number parts
+VNUM1=${CURRENT_VERSION_PARTS[0]}
+VNUM2=${CURRENT_VERSION_PARTS[1]}
+VNUM3=${CURRENT_VERSION_PARTS[2]}
+
+#BASED on input, update version number
+if [[ $VERSION == 'major' ]]
+then
+  VNUM1=v$((VNUM1+1))
+elif [[ $VERSION == 'minor' ]]
+then
+  VNUM2=$((VNUM2+1))
+elif [[ $VERSION == 'patch' ]]
+then
+  VNUM3=$((VNUM3+1))
+else
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  exit 1
+fi
+
+# create new tag
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
+
+# get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+
+# only tag if no tag already
+if [ -z "$NEEDS_TAG" ]; then
+  echo "Tagged with $NEW_TAG"
+  git tag $NEW_TAG
+  git push --tags
+  git push
+else
+  echo "Already a tag on this commit"
+fi
+
+echo ::set-output name=git-tag::$NEW_TAG
+
+exit 0
